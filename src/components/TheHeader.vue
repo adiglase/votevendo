bb8e01cec71654942ab5825822c79de2f320c0ec
Adi Gunawan Lase <adiglase6@gmail.com>
2024-05-22 19:58:19 +0700

    Reduce space between text

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -2,11 +2,11 @@
 
 <template>
   <div id="title">
-    <h1 class="text-6xl font-bold">
+    <h1 class="text-6xl font-bold mt-0 mb-2">
       VendoVote<br />
       <span class="text-primary font-bold">{{ $route.meta.pageName }}</span>
     </h1>
-    <p class="text-700 my-2">
+    <p class="text-700 my-0">
       Participate in vendor selection with confidence using our secure platform.
     </p>
     <p class="text-500 text-sm">0x620...e292F7 | Connected With Metamask</p>
