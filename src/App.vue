903ce11973fe380b17b694926b43a8ad2b1b8400
Adi Gunawan Lase <adiglase6@gmail.com>
2024-05-30 19:24:55 +0700

    Fix false toast message when metamask disconnected

--- src/App.vue
+++ src/App.vue
@@ -10,13 +10,15 @@ import router from './router'
 const toast = useToast()
 
 window.ethereum.on('accountsChanged', function (accounts) {
-    if (accounts.length === 0) router.push({ name: 'home' })
-    toast.add({
-        severity: 'success',
-        summary: 'Disconnected',
-        detail: 'Metamask wallet disconnected',
-        life: 3000
-    })
+    if (accounts.length === 0) {
+        router.push({ name: 'home' })
+        toast.add({
+            severity: 'success',
+            summary: 'Disconnected',
+            detail: 'Metamask wallet disconnected',
+            life: 3000
+        })
+    }
 })
 </script>
 
