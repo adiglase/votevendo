78418b93d448adc6ade61195a57c38fd14a9119c
Adi Gunawan Lase <adiglase6@gmail.com>
2024-08-19 23:09:17 +0700

    Update contract address

--- src/env.js
+++ src/env.js
@@ -1,4 +1,4 @@
 const ownerAddress = '0x6201CE287DD75BfF199Fb96f8E286db94fe292F7'
-const contractAddress = '0xB07C5857719bA5E30E242c9C20fb4343BA86811C'
+const contractAddress = '0x183A66985Dc7B008b68150cb23643D2b115e6eE4'
 
 export { ownerAddress, contractAddress }
