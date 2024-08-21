84ee93c162a1ae97ba8b36c4c8285b1a1e6d5a9d
Adi Gunawan Lase <adiglase6@gmail.com>
2024-08-21 10:13:33 +0700

    Change smart contract address

--- src/env.js
+++ src/env.js
@@ -1,4 +1,4 @@
 const ownerAddress = '0x6201CE287DD75BfF199Fb96f8E286db94fe292F7'
-const contractAddress = '0x183A66985Dc7B008b68150cb23643D2b115e6eE4'
+const contractAddress = '0x765536EAbc80b9a20Da183EF85Bd2dd50d24Beb5'
 
 export { ownerAddress, contractAddress }
